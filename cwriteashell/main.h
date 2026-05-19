ace246461f29b690ba1723cd6228b2a4fb04ddbe
Mohammad <mohammad.dawood.nazer@tuhh.de>
2026-05-19 14:52:45 +0200

    init

---
+++ cwriteashell/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+/*---MACROS----*/
+#define LSH_RL_BUFSIZE 1024
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+/*---END MACROS----*/
+
+/*---DECLARATIONS---*/
+
+/*---END DECLARATIONS---*/
+
+
+/*---PROTOTYPES---*/
+/*---END PROTOTYPES---*/
+
+#endif /*MAIN_H*/
