6be4989feef6e72adc6b22f0c917dde42afed8e4
Mohammad <shafi.bahrami.2015@gmail.com>
2026-06-28 21:53:55 +0200

    hash table functions

--- chashtables/hash_table.c
+++ chashtables/hash_table.c
@@ -31,15 +31,92 @@ static void hs_del_item(ht_item_t *item);
  **/
 static int ht_get_hash(const char *string, const int num_buckets, const int attempt);
 
+// sentinel for a deleted hash item
+static ht_item_t HT_DELETED_ITEM = {NULL, NULL};
 
-static ht_hashtable_t * ht_new_sized(const int base_size){
-   ht_hashtable_t * ht = xmalloc(sizeof());
+/**
+   @brief resizes the hash table to the next primer number close to the size "base_size" allocating the same size for "items" array
+   @param args "base_size" as the new base_size of the scaled hash table
+   @return pointer to the newly scalled hash table
+ **/
+static ht_hashtable_t *ht_new_sized(const int base_size)
+{
+   ht_hashtable_t *ht = xmalloc(sizeof(ht_hashtable_t *));
+   ht->base_size = base_size;
+   ht->size = next_prime(ht->base_size);
+   ht->count;
+   ht->items = xcalloc((size_t)ht->size, sizeof(ht_item_t *));
+
    return ht;
 }
 
+/**
+   @brief helper to initiate a hash table with the inital base_size
+   @param args none
+   @return pointer to newly allocated hash table
+ **/
+ht_hashtable_t *ht_new()
+{
+   return ht_new_sized(HT_INITIAL_BASE_SIZE);
+}
 
-// sentinel for a deleted hash item
-static ht_item_t HT_DELETED_ITEM = {NULL, NULL};
+/**
+   @brief use ht_new_sized() to scale hash table to new base size "base size", and then coping all the member of newly scalled hash table to original
+   @param args "ht" as original hash table to scale, "base_size" as new size towards scalling
+   @return void
+ **/
+static void ht_resize(ht_hashtable_t *ht, const int base_size)
+{
+   if (base_size < HT_INITIAL_BASE_SIZE)
+      return;
+
+   ht_hashtable_t *new_ht = ht_new_sized(base_size);
+   for (int i = 0; i < ht->size; i++)
+   {
+      ht_item_t *item = ht->items[i];
+      if (item != NULL || item != &HT_DELETED_ITEM)
+      {
+         ht_insert(new_ht, item->key, item->val);
+      }
+   }
+
+   ht->base_size = new_ht->base_size;
+   ht->count = new_ht->count;
+
+   // swapping via temp, so the temporal storage new_ht can be freed
+   int temp_size = new_ht->size;
+   ht->size = temp_size;
+   new_ht->size = temp_size;
+
+   ht_item_t **temp_items = new_ht->items;
+   ht->items = temp_items;
+   new_ht->items = temp_items;
+
+   ht_del_item(new_ht);
+}
+
+/**
+   @brief scales the size of hash table to 2 times its current size, if load > 70%
+   @param args "ht" to scale
+   @return void
+ **/
+static void ht_resize_up(ht_hashtable_t *ht)
+{
+   const new_ht_size = (size_t)(ht->base_size * 2);
+   ht_resize(ht, new_ht_size);
+}
+
+/**
+   @brief scales the size of hash table to half times its current size, if load < 30%
+   @param args "ht" to scale
+   @return void
+ **/
+static void ht_resize_down(ht_hashtable_t *ht)
+{
+   const new_ht_size = (size_t)(ht->base_size / 2);
+   ht_resize(ht, new_ht_size);
+}
+
 /**************************STATIC FUNCTIONS************************************** */
 
 int ht_hash(const char *s, const int a, const int m)
@@ -98,6 +175,11 @@ void ht_del_hashtable(ht_hashtable_t *ht)
 
 void ht_insert(ht_hashtable_t *ht, const char *key, const char *value)
 {
+   // taking resizing in count, if current items array is occupied by >70%, scalling up the size of wanted
+   const int load = ht->count / ht->size;
+   if (load > 0.7)
+      ht_resize_up(ht);
+
    ht_item_t *item = ht_item_new(key, value);
    int index = ht_get_hash(item->key, ht->size, 0);
    ht_item_t *curr_item = ht->items[index];
@@ -143,6 +225,11 @@ char *ht_search(ht_hashtable_t *ht, const char *key)
 static ht_item_t HT_DELETED_ITEM = {NULL, NULL};
 void ht_delete(ht_hashtable_t *ht, const char *key)
 {
+   // taking resizing in count, if current items array is occupied by >70%, scalling up the size of wanted
+   const int load = ht->count / ht->size;
+   if (load < 0.3)
+      ht_resize_down(ht);
+
    int index = ht_get_hash(key, ht->size, 0);
    ht_item_t *item = ht->items[index];
    int i = 1;
