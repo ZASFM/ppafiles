e33c36a43a26702520ab67bc8c1e34b9e598ec75
Mohammad <shafi.bahrami.2015@gmail.com>
2026-06-28 22:06:23 +0200

    fix. Closes #4

--- chashtables/prime.h
+++ chashtables/prime.h
@@ -4,6 +4,17 @@
 #define HT_INITIAL_BASE_SIZE  50
 /*********************MACROS*********************** */
 
+/**
+   @brief brute force method to check is a number is prime
+   @param args "x" as number to check
+   @return "1" if prime, "0" if undef, "-1" if not prime
+ **/
 int is_prime(const int x);
+
+/**
+   @brief check the next closes prime to a certain number
+   @param args "x" as number to check
+   @return closest prime to x
+ **/
 int next_prime(int x);
 
